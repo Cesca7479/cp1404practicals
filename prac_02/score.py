8065e21a540f143b97f25686e7b86fbddde070ac
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-09-23 23:18:46 +1000

    Refactor score program by adding functions, add extra features to ensure SRP, increase efficiency

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,33 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+from random import randint
+
+
+def main():
+    """ Receive score from user, provide result of score. Also prints result of a random score """
+    score = float(input("Enter score: "))
+    print(determine_result(score))
+    # The following two lines can be reduced to 1,
+    # dependent on if the random score needs to be stored and redisplayed.
+    # done simply with the following:
+    # print(f"Result of random score: {determine_result(randint(0, 100)}")
+    random_score = randint(0, 100)
+    print(f"Result of random score ({random_score}): {determine_result(random_score)}")
+
+
+def determine_result(score):
+    """ Determine result based off score provided """
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    if score >= 90:
+        return "Excellent"
+    if score >= 50:
+        return "Passable"
+    return "Bad"
+
+    # All 'elif's were replaced with 'if' statements, and else was removed as the function will have
+    # already returned to main() if any of the previous statements were true.
+
+
+main()
