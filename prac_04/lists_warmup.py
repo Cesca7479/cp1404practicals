518cdd1566bfef7f9d6eaab6c3a44f477402f249
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-10-07 11:21:03 +1000

    Add lists_warmup program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+"""
+View how indexing works in python
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# My Answers:
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = [1]
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"  # Warning because it is an unexpected type
+numbers[-1] = 1
+print(numbers[2::])
+print(9 in numbers)
