f9bfdce741635cd1d97b73c5bc45ceb66c1faf45
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-10-07 16:39:32 +1000

    Add completed quick picks program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,18 @@
+"""
+Ask user for a number of "quick picks", generate that many lines of output
+"""
+
+from random import randint
+
+MINIMUM = 1
+MAXIMUM = 45
+
+number_of_quick_picks = int(input("How many quick picks? "))
+for i in range(number_of_quick_picks):
+    quick_picks = []
+    for k in range(6):
+        number = randint(MINIMUM, MAXIMUM)
+        while number in quick_picks:
+            number = randint(MINIMUM, MAXIMUM)
+        quick_picks.append(number)
+    print(" ".join(f"{number:2}" for number in sorted(quick_picks)))
