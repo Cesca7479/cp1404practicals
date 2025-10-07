9398343815c084839e04f914e0c6ddbae926275b
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-10-07 12:48:49 +1000

    Add security checker to list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,6 +3,7 @@ Prompt user for 5 numbers, storing them in a list.
 Output information about the numbers
 """
 
+# Basic list operations
 numbers = []
 for i in range(5):
     number = int(input("Number: "))
@@ -12,3 +13,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
