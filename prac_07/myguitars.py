14b2a3eefcce476270756151623be00dbaaf5f69
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-04 15:32:54 +1000

    Add functions to save to file and get new guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,14 +8,42 @@ FILENAME = "guitars.csv"
 
 def main():
     """Load guitars from a file, sort and print to screen."""
-    guitars = sorted(load_guitars())
+    guitars = sorted(load_guitars(FILENAME))
     display_guitars(guitars)
+    guitars = get_guitars(guitars)
+    display_guitars(guitars)
+    save_guitars(guitars, FILENAME)
 
 
-def load_guitars():
+def get_guitars(guitars):
+    """Get new guitars from user"""
+    name = input("Name: ")
+    while name != "":
+        year = get_valid_year()
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ")
+    return sorted(guitars)
+
+
+def get_valid_year():
+    is_valid = False
+    while not is_valid:
+        try:
+            year = int(input("Year: "))
+            # can make sure year doesn't exceed CURRENT YEAR...
+            is_valid = True
+        except ValueError:
+            print("Invalid year")
+    return year
+
+
+def load_guitars(filename):
     """Load guitars from a file, store in guitars list."""
     guitars = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
             parts = line.strip().split(",")
             year = int(parts[1])
@@ -31,6 +59,13 @@ def display_guitars(guitars):
     longest_cost_length = max(len(str(f"{guitar.cost:,.2f}")) for guitar in guitars)
     for guitar in guitars:
         print(f"{guitar.name:{longest_name_length}} ({guitar.year}) : ${guitar.cost:{longest_cost_length},.2f}")
+
+
+def save_guitars(guitars, filename):
+    """Save guitars to file in csv format"""
+    with open(filename, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 main()
