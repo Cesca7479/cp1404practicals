67894548d590b53675734790d382389d5b2727a7
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-07 01:20:43 +1000

    Add load_file function to project management program

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,18 +8,18 @@ import datetime
 class Project:
     """Contain information on project objects."""
 
-    def __init__(self, name="", start_date=datetime.date.today(), priority=0, cost=0.0, status=0):
+    def __init__(self, name="", start_date=datetime.date.today(), priority=0, cost=0.0, completion=0):
         """Initialise Project attributes."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost = cost
-        self.status = status
+        self.completion = completion
 
     def __repr__(self):
         """Return attributes in a formatted string."""
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: {self.cost}, completion: "
-                f"{self.status}%")
+                f"{self.completion}%")
 
     def __lt__(self, other):
         """Determine an order of projects, by priority."""
@@ -27,7 +27,7 @@ class Project:
 
     def is_complete(self):
         """Determine if a project is complete."""
-        return self.status == 100
+        return self.completion == 100
 
     def is_after(self, date):
         """Determine if a project was created after a certain date"""
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,6 +1,44 @@
 """
-
+Run through a menu that allows a user to load, display, filter, add and update projects
 Estimated: 3 hrs
 Actual:
 """
 
+import datetime
+
+from prac_07.project import Project
+
+MENU = ("- (L)oad projects"
+        "- (S)ave projects"
+        "- (D)isplay projects"
+        "- (F)ilter projects"
+        "- (A)dd new project"
+        "- (U)pdate project"
+        "- (Q)uit")
+DEFAULT_FILENAME = "projects.txt"
+
+
+def main():
+    """Load default projects file, cycle through a menu, provide option to save file upon completion."""
+    projects = load_file(DEFAULT_FILENAME)
+    print("Welcome to Pythonic Project Management")
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
+
+
+def load_file(filename):
+    """Load contents of file and store Projects in a list"""
+    projects = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+            priority = int(parts[2])
+            cost = float(parts[3])
+            completion = float(parts[4])
+            project = Project(parts[0], start_date, priority, cost, completion)
+            projects.append(project)
+    return projects
+
+
+main()
