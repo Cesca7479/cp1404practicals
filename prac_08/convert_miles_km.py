06ad9018c9d14a8b3f2ccfc7582c107688c9aec4
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-15 14:45:56 +1000

    Add docstrings, properly utilise the StringProperty()

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,5 +1,5 @@
 """
-Build and use the kv app to convert miles to km
+Build and use the kv app to convert miles to km.
 """
 from kivy.app import App
 from kivy.lang import Builder
@@ -9,25 +9,29 @@ CONVERSION_CONSTANT = 1.60934
 
 
 class ConvertMilesKM(App):
-    output = StringProperty()
+    """Run an app that will convert Miles to km."""
+    output_km = StringProperty()
 
     def build(self):
+        """Build the app and initialise text."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
-        self.output = "Enter a number in miles to convert to kilometres"
         return self.root
 
     def convert_miles_km(self, text):
+        """Convert Miles to km."""
         result = self.convert_to_number(text) * CONVERSION_CONSTANT
-        self.root.ids.output_label.text = str(result)
+        self.output_km = str(result)
 
     def handle_increment(self, input_text, increment):
+        """Handle up and down buttons by incrementing the input value."""
         result = self.convert_to_number(input_text) + increment
         self.root.ids.input_number.text = str(result)
         self.convert_miles_km(result)
 
     @staticmethod
     def convert_to_number(text):
+        """Convert input text to a number."""
         try:
             return float(text)
         except ValueError:
