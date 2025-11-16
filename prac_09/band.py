00b9ddd94eff02aec3276c98ea4d7c1652e460a3
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 23:49:30 +1000

    Add Band class

--- prac_09/band.py
+++ prac_09/band.py
@@ -8,4 +8,19 @@ class Band:
 
     def __init__(self, name):
         """Initialise Band Object with a name and empty list of musicians."""
+        self.name = name
         self.musicians = []
+
+    def __str__(self):
+        """Return Band name and musicians in the band."""
+        return f"{self.name} ({self.musicians})"
+
+    def add(self, musician):
+        """Add Musician to the Band."""
+        return self.musicians.append(musician)
+
+    def play(self):
+        """Return a string stating the musicians who are playing in the band."""
+        if not self.musicians:
+            return "There are no musicians!"
+        return "\n".join(musician.play() for musician in self.musicians)
