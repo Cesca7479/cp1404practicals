d6d37cfaac3baf42b6eb5cc3e613593ae72152c0
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 22:37:09 +1000

    Complete Unreliable Car class

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,26 @@
+"""
+Create Unreliable Car Class, derived from Car.
+"""
+
+from car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """Represent an Unreliable Car Object."""
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        """Return a string like a Car but with car unreliability."""
+        return f"{super().__str__()}, reliability: {self.reliability}%"
+
+    def drive(self, distance):
+        if randint(0, 100) < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
+
