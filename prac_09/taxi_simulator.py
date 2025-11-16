997315e7a6898dc160d91349ce99d641cca4bd14
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-17 00:12:19 +1000

    Add drive taxi function and main function

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,48 @@
+"""
+Use Taxi and SilverServiceTaxi Classes to run a taxi simulator program
+"""
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d(rive"
+
+
+def main():
+    """Run Taxi Simulator."""
+    print("Let's drive!")
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    bill = 0
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            drive_taxi(current_taxi)
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${bill}")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+
+def choose_taxi(taxis):
+    """Get taxi choice from user."""
+    print("Taxis available:")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def drive_taxi(current_taxi):
+    """Drive chosen taxi."""
+    if not current_taxi:
+        print("You need to choose a taxi before you can drive")
+    else:
+        distance = int(input("Drive how far? "))
+        current_taxi.drive(distance)
+        print(f"Your {current_taxi.name} trip cost you ${current_taxi.get_fair()}")
+
+
+main()
