cfbec27d84579d690863e721cf879a989459c47d
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 22:51:27 +1000

    Add Silver Service Taxi Class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,22 @@
+"""
+Silver Service taxi Class, inherited from Taxi.
+"""
+
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Represent Silver Service Taxi Class."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness=0.0):
+        """Initialise Silver Service Taxi Class."""
+        super().__init__(name, fuel)
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        """Return the price for the Silver Service Taxi Trip."""
+        return super().get_fare() + self.flagfall
