ff8d846ce7480d7154b061353fc91ae2337c1a94
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 20:21:18 +1000

    Complete Class Variables suggestions

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,7 +6,7 @@ from taxi import Taxi
 
 def main():
     """Test Taxi."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(f"{my_taxi}, current fair: ${my_taxi.get_fare():.2f}")
     my_taxi.start_fare()
