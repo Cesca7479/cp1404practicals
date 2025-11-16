2cf8f8f73d8e2d87b8c7f083ef6cb5946b257074
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 23:04:10 +1000

    Adjust string for Car class

--- prac_09/car.py
+++ prac_09/car.py
@@ -14,7 +14,7 @@ class Car:
 
     def __str__(self):
         """Return a string representation of a Car object."""
-        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
+        return f"{self.name}, fuel={self.fuel}, odo={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
