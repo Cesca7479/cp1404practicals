70b87dbb1603f967249fb70bbdaef4197334ae74
jd117390 <francesca.adcock@my.jcu.edu.au>
2025-11-16 23:14:21 +1000

    Change Taxi class to round fairs to 10c

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -10,7 +10,7 @@ def main():
     assert str(hummer_taxi) == "Hummer, fuel=200, odo=0, 0km on current fare, $4.92/km plus flagfall of $4.50"
     silver_taxi = SilverServiceTaxi("Silver Taxi", 100, 2)
     silver_taxi.drive(18)
-    assert silver_taxi.get_fare() == 48.78
+    assert silver_taxi.get_fare() == 48.80
 
 
 main()
